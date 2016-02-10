d015d4347d0daffa613f227e3c12ba18f2ca1ddd
Daniel Flanagan <daniel@lytedev.io>
2016-02-10 00:42:35 -0600

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="jade">
+  #app
+    img.logo(src="./assets/logo.png")
+    hello
+</template>
+
+<script lang="coffee">
+  hello = require './components/Hello'
+
+  module.exports =
+    components:
+      hello: hello
+</script>
+
+<style lang="stylus">
+  @import url(https://fonts.googleapis.com/css?family=Lato:400,100,300,700)
+
+  body
+    font-family Lato, sans-serif
+</style>
